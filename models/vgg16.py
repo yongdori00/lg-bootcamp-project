a2da31133b6a0dd8b2b2f1aae6519d64b5598e19
Jiye Kim <jiye.kim@lge.com>
2023-11-01 16:50:27 +0900

    vgg model update: image gen from df

--- models/vgg16.py
+++ models/vgg16.py
@@ -2,6 +2,7 @@ import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
 import os
+import pandas as pd
 
 from tensorflow.keras.layers import Input
 from tensorflow.keras.layers import Conv2D
@@ -15,17 +16,20 @@ from tensorflow.keras.applications import VGG16
 
 ### Parameters
 
-# Image Params
+# Data Params
 IMG_WIDTH = 64
 IMG_HEIGHT = 64
 IMG_CHANNELS = 3
+IMG_PATH = "../data/images"
+CSV_PATH = "../data/labels.csv"
+DF_XCOL = "filenames"
+DF_YCOL = ["row", "col"]
 
 # Grid Params
 GRID_ROWS = 6
 GRID_COLS = 8
 
 # Model Params
-IMG_PATH = "../images"
 RESIZED_WIDTH = IMG_WIDTH
 RESIZED_HEIGHT = IMG_HEIGHT
 LABELS = GRID_ROWS * GRID_COLS
@@ -36,6 +40,9 @@ VAL_STEPS = 6
 DENSE_UNITS = 1024
 
 
+### Dataframe 불러오기
+df = pd.read_csv(CSV_PATH)
+
 
 ### Data Generation
 image_gen = tf.keras.preprocessing.image.ImageDataGenerator(
@@ -44,24 +51,35 @@ image_gen = tf.keras.preprocessing.image.ImageDataGenerator(
         brightness_range=(0.2, 1.0)   # Range for picking a brightness shift value from. 0 == black, 1 == normal
         )
 
-train_data_gen = image_gen.flow_from_directory(
-                      directory=IMG_PATH,         # 디렉터리 지정
+train_data_gen = image_gen.flow_from_dataframe(
+                      dataframe=df,         # 디렉터리 지정
+                      directory=IMG_PATH,
+                      x_col=DF_XCOL,
+                      y_col=DF_YCOL,
                       batch_size=BATCH_N,         # 한번에 생성할 데이터의 크기 설정
                       target_size=(RESIZED_WIDTH, RESIZED_HEIGHT),                     # 변경될 이미지 데이터의 크기
-                      # classes = [[i//GRID_COLS, i%GRID_COLS] for i in range(LABELS)],  # 클래스 번호 부여: 디렉터리 순
-                      class_mode = 'categorical', # Label을 숫자로 표기
+                      # classes = [(i//GRID_COLS, i%GRID_COLS) for i in range(LABELS)],  # 클래스 번호 부여: 디렉터리 순
+                      class_mode = 'multi_output', # Label을 숫자로 표기
                       subset='training'           # Training 용 데이터: 전체의 80%
                       )
 
-test_data_gen = image_gen.flow_from_directory(
-                      directory=IMG_PATH,         # 디렉터리 지정
+test_data_gen = image_gen.flow_from_dataframe(
+                      dataframe=df,         # 디렉터리 지정
+                      directory=IMG_PATH,
+                      x_col=DF_XCOL,
+                      y_col=DF_YCOL,
                       batch_size=BATCH_N,         # 한번에 생성할 데이터의 크기 설정
                       target_size=(RESIZED_WIDTH, RESIZED_HEIGHT),                     # 변경될 이미지 데이터의 크기
-                      # classes = [[i//GRID_COLS, i%GRID_COLS] for i in range(LABELS)],  # 클래스 번호 부여: 디렉터리 순
-                      class_mode = 'categorical', # Label을 숫자로 표기
+                      # classes = [(i//GRID_COLS, i%GRID_COLS) for i in range(LABELS)],  # 클래스 번호 부여: 디렉터리 순
+                      class_mode = 'multi_output', # Label을 숫자로 표기
                       subset='validation'         # Testing 용 데이터: 전체의 20%
                       )
 
+print("******************************")
+train_data_t, train_labels_t = next(train_data_gen)
+print(np.array(train_data_t).shape)
+print(np.array(train_labels_t).shape)
+print(train_labels_t)
 
 ### Load VGG16 Model
 model = VGG16(
@@ -69,7 +87,8 @@ model = VGG16(
     include_top=False,
     input_tensor=Input(shape=(RESIZED_WIDTH, RESIZED_HEIGHT, IMG_CHANNELS))
     )
-# model.summary()
+print('original VGG16')
+# print(model.summary())
 
 
 ### Set trainable option
@@ -84,28 +103,28 @@ x = Flatten()(model.layers[-1].output)
 x = Dense(DENSE_UNITS, activation='relu')(x)
 x = BatchNormalization()(x)
 
-# 첫번째 출력: COL
-col_out = Dense(GRID_COLS, activation='softmax', name='col_out')(x)
-
-# 두번째 출력: ROW
+# 첫번째 출력: ROW
 row_out = Dense(GRID_ROWS, activation='softmax', name='row_out')(x)
 
-# 최종 모델
-model_out = tf.keras.models.Model(inputs=model.input, outputs=[col_out, row_out])
+# 두번째 출력: COL
+col_out = Dense(GRID_COLS, activation='softmax', name='col_out')(x)
 
-# model_fine.summary()
+# 최종 모델
+model_out = tf.keras.models.Model(inputs=model.input, outputs=[row_out, col_out])
+print('fine tuned model')
+print(model_out.summary())
 
 
 ### Loss function
-loss_weights = {'col_out': 1.0, 'row_out': 1.0}  # 각 출력에 대한 가중치
-loss = {'col_out': 'categorical_crossentropy', 'row_out': 'categorical_crossentropy'}  # 각 출력에 대한 손실 함수
+# loss_weights = {'row_out': 1.0, 'col_out': 1.0}  # 각 출력에 대한 가중치
+# loss = {'row_out': 'categorical_crossentropy', 'col_out': 'categorical_crossentropy'}  # 각 출력에 대한 손실 함수
 
 
 ### Model compile
 model_out.compile(
         optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
-        loss=loss,
-        loss_weights=loss_weights,
+        loss='categorical_crossentropy',
+        # loss_weights=loss_weights,
         metrics=['accuracy'])
 
 
