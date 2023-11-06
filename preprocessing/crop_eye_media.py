362ea9794b2a7f21d47664c4bc2acdcd2e818541
yongdori00 <yongdori98@gmail.com>
2023-11-06 15:58:16 +0900

    modified crop_eye_mdeia.py

--- preprocessing/crop_eye_media.py
+++ preprocessing/crop_eye_media.py
@@ -7,7 +7,7 @@ mp_drawing = mp.solutions.drawing_utils
 
 # 특정 경로에서 이미지 불러오기
 directory_path = 'images/'  # 이미지 파일 경로를 지정합니다.
-cropped_path = 'cropped_eye/'
+cropped_path = 'cropped_real_eye/'
 image_directory = [f for f in os.listdir(directory_path)]
 print(image_directory)
 
@@ -33,22 +33,18 @@ for height_directory_path in image_directory:
                     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
                     if results.detections:
                         for detection in results.detections:
-                            mp_drawing.draw_detection(image, detection)
 
-                    cv2.imshow("whow", cv2.resize(image, None, fx=0.5, fy=0.5))
+                            keypoints = detection.location_data.relative_keypoints
+                            right_eye = keypoints[0]
+                            left_eye = keypoints[1]
 
-                    keypoints = detection.location_data.relative_keypoints
-                    right_eye = keypoints[0]
-                    left_eye = keypoints[1]
+                            h, w, _ = image.shape
+                            right_eye = (int(right_eye.x * w), int(right_eye.y * h))
+                            left_eye = (int(left_eye.x * w), int(left_eye.y * h))
+                            
+                            right_eye_image = image[right_eye[1] - 10:right_eye[1] + 10, right_eye[0] - 25:right_eye[0] + 25]
+                            left_eye_image = image[left_eye[1] - 10:left_eye[1] + 10, left_eye[0] - 25:left_eye[0] + 25]
 
-                    h, w, _ = image.shape
-                    right_eye = (int(right_eye.x * w), int(right_eye.y * h))
-                    left_eye = (int(left_eye.x * w), int(left_eye.y * h))
-                    
-                    right_eye_image = image[right_eye[1] - 10:right_eye[1] + 10, right_eye[0] - 25:right_eye[0] + 25]
-                    left_eye_image = image[left_eye[1] - 10:left_eye[1] + 10, left_eye[0] - 25:left_eye[0] + 25]
-
-                    print(right_eye_image.shape)
                 # cv2.rectangle(image, (x + ex, y + ey), (x + ex + ew, y + ey + eh), (0, 255, 0), 2)
 
                 # 두 이미지를 수평으로 합칩니다.
